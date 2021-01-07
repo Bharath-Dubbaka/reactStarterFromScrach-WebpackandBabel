d8125cdb021f0b965a4f9570c8939951ad915849
Bharath Dubbaka <72569262+Bharath-Dubbaka@users.noreply.github.com>
2021-01-08 02:01:48 +0530

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,30 +1,9 @@
 import React, { Component } from 'react';
-import Button from '@material-ui/core/Button';
-import Search from '@material-ui/icons/Search'
-import TextField from '@material-ui/core/TextField'
-import '@material-ui/core/styles';
-
 
 class Home extends Component {  
     render (){
     return (
     <div>
-        <form>
-            <TextField
-                variant='outlined' 
-                size='small'
-                color='secondary'
-                placeholder='eg: Java AND React'
-            />
-            <Button 
-                // href='#' 
-                startIcon={<Search/>}
-                // size='small'
-                variant='contained' 
-                color='primary'>
-                Search
-            </Button>
-        </form>
         <h4>
             container
         </h4>
@@ -34,7 +13,7 @@ class Home extends Component {
 
 }
 
-
+    
 
 export default Home;
 
